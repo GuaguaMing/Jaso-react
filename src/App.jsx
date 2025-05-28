06811ff520a3c8e670a66aeb32c6c62622b6c8af
GuaguaMing <pantxiketsai499@gmail.com>
2025-05-28 16:16:39 +0800

    0528ming-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import AppHome from './1home/AppHome';
 
 function App() {
