60b267a8bcb716fe0b19ad4c9ed54c57f74454cb
co-004 <cocolin40401234@gmail.com>
2025-06-05 18:14:28 +0800

    0605co-1-3

--- src/1components/NavbarHidden.jsx
+++ src/1components/NavbarHidden.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from "react-router-dom";
 import styles from '../../scss/pages/1component/navbarhidden.module.scss';
 
 const NavbarHidden = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userName, setUserName] = useState("會員");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const name = localStorage.getItem("userName");
+    if (token) {
+      setIsLoggedIn(true);
+      setUserName(name || "會員");
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
   return (
     <header>
       <div className={styles.navHidden}>
-        <a className={styles.logo2}><Link to="/">
-        <img src={`${import.meta.env.BASE_URL}assets/nav-logo.svg`} alt="logo"/>
-          {/* <img src={navLogo} alt="logo" /> */}</Link>
+        <a className={styles.logo2}>
+          <Link to="/">
+            <img src={`${import.meta.env.BASE_URL}assets/nav-logo.svg`} alt="logo" />
+          </Link>
         </a>
+
         <div className={styles.navbar2}>
           <div className={styles.navCenter2}>
             <ul>
@@ -18,11 +34,22 @@ const NavbarHidden = () => {
               <li><Link to="/quiz">營養素算</Link></li>
             </ul>
           </div>
+
           <div className={styles.navRight2}>
-            <div className={styles.member2}><Link to="/memberCenter">會員</Link></div>
-            <a className={styles.shopList2}><Link to="/cart">
-              <img src={`${import.meta.env.BASE_URL}assets/shop-list.svg`} alt="" /></Link> 
-            </a>
+            <div className={styles.member2}>
+              {isLoggedIn
+                ? <Link to="/memberCenter">Hi, {userName}</Link>
+                : <Link to="/login">登入</Link>
+              }
+            </div>
+
+            {isLoggedIn && (
+              <a className={styles.shopList2}>
+                <Link to="/cart">
+                  <img src={`${import.meta.env.BASE_URL}assets/shop-list.svg`} alt="cart" />
+                </Link>
+              </a>
+            )}
 
             <div className={styles.beanShape2}>
               <div className={styles.goToShop2}><Link to="/shop">素購</Link></div>
